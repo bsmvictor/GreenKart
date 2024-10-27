603dd8290f46b6819c2cda67c09b83e148fc705d
Victor Boaventura <victor.boaventura@gec.inatel.br>
2024-10-27 16:50:57 -0300

    Update Project

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+    viewportWidth: 1366,
+    viewportHeight: 768,
+  },
+});
